8e1eaaae02862e71df6dfb5714c896edf61ae252
Fabio Silveira <fabio.eds@hotmail.com>
2025-05-02 08:46:36 -0700

    AddedCode

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react"
+
+export default function App() {
+
+  const [formData, setFormData] = useState({
+    nome: "",
+    sobrenome: "",
+    cidade: "",
+    rua: "",
+    bairro: "",
+    numero: ""
+  })
+
+  function handleChange({ target }) {
+    const { name, value } = target
+    setFormData({
+      ...formData, [name]: value
+    })
+  }
+
+  function handleClick() {
+    console.log(formData)
+  }
+
+  return (
+    <>
+
+    {formData.nome} {formData.sobrenome} {formData.cidade} {formData.rua} {formData.bairro} {formData.numero}
+      <input
+        onChange={handleChange}
+        name="nome"
+        type="text"
+        placeholder="Nome"
+        value={formData.nome}
+      />
+      <input
+        onChange={handleChange}
+        name="sobrenome"
+        type="text"
+        placeholder="sobrenome"
+        value={formData.sobrenome}
+      />
+      <input
+        onChange={handleChange}
+        name="cidade"
+        type="text"
+        placeholder="cidade"
+        value={formData.cidade}
+      />
+      <input
+        onChange={handleChange}
+        name="rua"
+        type="text"
+        placeholder="rua"
+        value={formData.rua}
+      />
+      <input
+        onChange={handleChange}
+        name="bairro"
+        type="text"
+        placeholder="bairro"
+        value={formData.bairro}
+      />
+      <input
+        onChange={handleChange}
+        name="numero"
+        type="text"
+        placeholder="numero"
+        value={formData.numero}
+      />
+
+      <button onClick={handleClick}>Enviar</button>
+
+    </>
+  )
+}
