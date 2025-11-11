9bb650defb42b7da3b6bcdefc91d3238db547d2d
Fabio Silveira <fabio.eds@hotmail.com>
2025-11-11 13:10:49 -0800

    updatedStyle

--- src/App.jsx
+++ src/App.jsx
@@ -24,57 +24,69 @@ export default function App() {
 
   return (
     <>
+      <h2 className="h2-home">Preview:</h2>
 
-    {formData.nome} {formData.sobrenome} {formData.cidade} {formData.rua} {formData.bairro} {formData.numero}
+      <div className="input-values-results">
 
-      <div className="div-body"> 
 
-      <input
-        onChange={handleChange}
-        name="nome"
-        type="text"
-        placeholder="Nome"
-        value={formData.nome}
-      />
-      <input
-        onChange={handleChange}
-        name="sobrenome"
-        type="text"
-        placeholder="sobrenome"
-        value={formData.sobrenome}
-      />
-      <input
-        onChange={handleChange}
-        name="cidade"
-        type="text"
-        placeholder="cidade"
-        value={formData.cidade}
-      />
-      <input
-        onChange={handleChange}
-        name="rua"
-        type="text"
-        placeholder="rua"
-        value={formData.rua}
-      />
-      <input
-        onChange={handleChange}
-        name="bairro"
-        type="text"
-        placeholder="bairro"
-        value={formData.bairro}
-      />
-      <input
-        onChange={handleChange}
-        name="numero"
-        type="text"
-        placeholder="numero"
-        value={formData.numero}
-      />
 
-      <button onClick={handleClick}>Enviar</button>
+        {formData.nome} {formData.sobrenome} {formData.cidade} {formData.rua} {formData.bairro} {formData.numero}
 
       </div>
+
+      <div className="div-body">
+
+        <input
+          onChange={handleChange}
+          name="nome"
+          type="text"
+          placeholder="Nome"
+          value={formData.nome}
+        />
+        <input
+          onChange={handleChange}
+          name="sobrenome"
+          type="text"
+          placeholder="sobrenome"
+          value={formData.sobrenome}
+        />
+        <input
+          onChange={handleChange}
+          name="cidade"
+          type="text"
+          placeholder="cidade"
+          value={formData.cidade}
+        />
+        <input
+          onChange={handleChange}
+          name="rua"
+          type="text"
+          placeholder="rua"
+          value={formData.rua}
+        />
+        <input
+          onChange={handleChange}
+          name="bairro"
+          type="text"
+          placeholder="bairro"
+          value={formData.bairro}
+        />
+        <input
+          onChange={handleChange}
+          name="numero"
+          type="text"
+          placeholder="numero"
+          value={formData.numero}
+        />
+
+        <button onClick={handleClick}>Enviar</button>
+
+
+
+      </div>
+
+      <p className="paragraph">Oi! Eu sou _______ _______ e moro em _______. Minha casa fica na rua _______,
+        número _______, no bairro _______.</p>
 
     </>
   )
